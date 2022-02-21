9d359662460d04025fd474f343f724b4d9f39c00
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2022-02-21 17:31:23 -0400

    refactor: Use container approach

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
+import { Button } from 'antd';
 import './App.css'
-import CustomLayout from './Layout/CustomLayout';
+import CustomLayout from './Container/CustomLayout';
+import { getCharacter } from './api/swapi';
+import CharacterLayout from './Container/CharacterLayout';
+import Character from './components/Character/Character';
 
-import Characters from './Container/Characters';
+const numberPeople = Array.from({ length: 10 }, (_, i) => i + 1);
 
 function App() {
+  const [character, setCharacter] = useState({});
+
+  const showCharacterHandler = async (number) => {
+    const character = await getCharacter(number);
+    setCharacter(character);
+  }
+
+  const setShowCharacterHandler = (num) => {
+    return showCharacterHandler.bind(null, num)
+  }
+
+  const buttons = numberPeople.map((num) => <><Button key={num}
+    onClick={setShowCharacterHandler(num)}> Character {num}</Button><br /></>);
+
   return (
     <CustomLayout>
-      <Characters />
+      <CharacterLayout buttons={buttons}>
+        <Character title="Character information" character={character} />
+      </CharacterLayout>
     </CustomLayout>
   );
 }
